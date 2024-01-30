8d163c73877ff13c76df3ab5948c052c3d8872c0
John-S-Reddick <154930148+John-S-Reddick@users.noreply.github.com>
2024-01-30 14:48:09 -0500

    Switching to python and tkinter

---
+++ CIS4930 Novel User Interfaces/Project 1/main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import tkinter as tk
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.grid()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.quitButton = tk.Button(self, text='Quit',
+            command=self.quit)
+        self.quitButton.grid()
+
+app = Application()
+app.master.title('Sample application')
+app.mainloop()
