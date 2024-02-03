70d1acb62f1461a346ed86421180e71d3d8c92e7
John-S-Reddick <154930148+John-S-Reddick@users.noreply.github.com>
2024-02-03 14:47:00 -0500

    Added drawing capability

--- CIS4930 Novel User Interfaces/Project 1/main.py
+++ CIS4930 Novel User Interfaces/Project 1/main.py
@@ -4,25 +4,29 @@ import numpy as np
 # Windows for Tkinter
 # https://likegeeks.com/python-gui-examples-tkinter-tutorial/
 
-# Canvas
+# Tkinter canvases
 # https://www.pythontutorial.net/tkinter/tkinter-canvas/
 
-# Mouse Events (In order of helpfulness)
-# 1. https://stackoverflow.com/questions/32289175/list-of-all-tkinter-events
-# 2. https://dafarry.github.io/tkinterbook/tkinter-events-and-bindings.htm
-# 3. https://tkinterexamples.com/events/mouse/
+# Tkinter window and Grid management:
+# https://www.pythonguis.com/tutorials/use-tkinter-to-design-gui-layout/
 
-# List of all tkinter events
-# https://stackoverflow.com/questions/32289175/list-of-all-tkinter-events
+# Tkinter events ranked by subjective usefulness
+# 1. https://stackoverflow.com/questions/32289175/list-of-all-tkinter-events
+# 2. https://www.tutorialexample.com/python-tkinter-bind-ctrlkey-python-tutorial/
+# 3. https://dafarry.github.io/tkinterbook/tkinter-events-and-bindings.htm
+# 4. https://tkinterexamples.com/events/mouse/
+
+# Tkinter components ranked by subjective usefulness
+# 1. https://www.geeksforgeeks.org/tkinter-cheat-sheet/
+
 
 # Writing to a file
 # https://www.geeksforgeeks.org/writing-to-file-in-python/
 
+
+
 # PyInstaller
 # https://pyinstaller.org/en/stable/
-
-
-
 
 
 
@@ -45,11 +49,11 @@ height = HEIGHT * WINDOW_SCALER
 
 # Create array of Zeros NumPy
 # https://numpy.org/doc/stable/reference/generated/numpy.zeros.html#numpy.zeros
-arr = np.zeros((1, 3),  dtype=int)
+list = []
 
-currentX = 0
-currentY = 0
 currentStroke = -1
+last_x = -1
+last_y = -1
 
 file1 = open(FILE_NAME, "w")
 
@@ -69,19 +73,58 @@ import tkinter as tk
 #Increases current stroke count
 def mouse_click(event):
     global currentStroke
+    global last_x
+    global last_y
+
     currentStroke = currentStroke + 1
-    print(arr)
+    last_x = -1
+    last_y = -1
+    print(list)
 
 def drag_handler(event):
     global currentStroke
-    #np.append(arr, np([event.x, event.y, currentStroke]), axis=0)
-    print([event.x, event.y, currentStroke])
+    global last_x
+    global last_y
+    if last_x == -1 or last_y == -1:
+        last_x = event.x
+        last_y = event.y
+    canvas1.create_line(last_x, last_y, event.x, event.y)
+    list.append(np.array([event.x, event.y, currentStroke]))
+    last_x = event.x
+    last_y = event.y
 
+def drag_handler(event):
+    global currentStroke
+    global last_x
+    global last_y
+    if last_x == -1 or last_y == -1:
+        last_x = event.x
+        last_y = event.y
+
+    canvas1.create_line(last_x, last_y, event.x, event.y)
+    list.append(np.array([event.x, event.y, currentStroke]))
+    last_x = event.x
+    last_y = event.y
+
+def undo(event):
+    print("bazinga")
+
+def redo(event):
+    print("agnizab")
+
+def submit():
+    print("bupsmit")
 
 
 root = tk.Tk()
 root.geometry('1920x1080')
 root.title('Canvas Demo')
+
+# Key Binding tutorial
+# https://www.tutorialexample.com/python-tkinter-bind-ctrlkey-python-tutorial/
+root.bind_all("<Control-Key-z>", undo)
+root.bind_all("<Control-Key-y>", redo)
+root.bind_all("<Return>", redo)
 
 canvas1 = tk.Canvas(root, width=canvasSize, height=canvasSize, bg='white')
 
@@ -91,7 +134,11 @@ canvas1.grid(column=0,row=0)
 canvas1.bind("<Button-1>", mouse_click)
 canvas1.bind("<B1-Motion>", drag_handler)
 
+
+
 canvas2 = tk.Canvas(root, width=canvasSize, height=canvasSize, bg='white')
 canvas2.grid(column=1,row=0)
 
+subButton = Button(root, text="Submit", command=submit)
+subButton.grid(column=0,row=1)
 root.mainloop()
