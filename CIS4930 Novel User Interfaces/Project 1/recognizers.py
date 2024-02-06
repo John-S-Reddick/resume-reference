1f291efd90aa3024c7436b74d62bbe1aa1901196
John-S-Reddick <154930148+John-S-Reddick@users.noreply.github.com>
2024-02-06 18:10:32 -0500

    finished scaffold for $1

--- CIS4930 Novel User Interfaces/Project 1/recognizers.py
+++ CIS4930 Novel User Interfaces/Project 1/recognizers.py
@@ -1,16 +1,18 @@
 import math
 import numpy as np
 
+
+PHI = .5 * (-1 + math.sqrt(5))
+PI_BY_4 = math.pi / 4       # θ = ±45°
+PI_BY_90 = math.pi / 90     # θ∆ = 2°
 # Point Access
 X = 0
 Y = 1
 STROKE = 2
 
-<<<<<<< Updated upstream
-=======
 K = [0,0]
+SIZE = 25
 
->>>>>>> Stashed changes
 # List aspects
 FIRST = 0
 DIMENSIONS = 3
@@ -24,28 +26,18 @@ def distance(p2, p1):
 def centroid(points):
     # Uses Numpy mean
     # https://numpy.org/doc/stable/reference/generated/numpy.mean.html
-<<<<<<< Updated upstream
-    c = np.array([X, Y, STROKE])
-=======
     c = np.array([[X, Y, STROKE]])
->>>>>>> Stashed changes
     c[X] = np.mean(points, where=[[True], [False], [False]])
     c[Y] = np.mean(points, where=[[False], [True], [False]])
     return c
 
-<<<<<<< Updated upstream
-class OneDollar():
-    # Psuedo code for class found here:
-    # https://github.com/John-S-Reddick/resume-reference/blob/ea908d0069c7ea63b2ef6da9cfee86df927a580d/CIS4930%20Novel%20User%20Interfaces/Project%201/Papers%20Cited/$1%20Recognizer.pdf
-=======
 #TODO Implement bounding_box
 def bounding_box(points):
     return [1,2]
 
 class OneDollar():
     # Psuedo code for class found here:
-    # 
->>>>>>> Stashed changes
+    #
     def resample(self, points, n):
         I = self.path_length(points) / (n - 1)
         D = 0
@@ -89,15 +81,9 @@ class OneDollar():
         return math.atan2(c[Y] - p[Y], c[X] - p[X])
 
     def rotate_by(self, points, w):
-<<<<<<< Updated upstream
-        new_points = np.array([X, Y, STROKE])
-        c = centroid(points)
-        q = np.array([X, Y, STROKE])
-=======
         new_points = np.array([[X, Y, STROKE]])
         c = centroid(points)
         q = np.array([[X, Y, STROKE]])
->>>>>>> Stashed changes
         for p in points:
             q[X] = (p[X] - c[X]) * math.cos(w) - (p[Y] - c[Y]) * math.sin(w) + c[X]
             q[Y] = (p[X] - c[X]) * math.sin(w) - (p[Y] - c[Y]) * math.cos(w) + c[Y]
@@ -105,15 +91,13 @@ class OneDollar():
 
         return np.delete(new_points, 0)
 
-<<<<<<< Updated upstream
-=======
-    def scale_to(self, points, size):
+    def scale_to(self, points):
         box = bounding_box(points)
         new_points = np.array([[X, Y, STROKE]])
         q = np.array([[X, Y, STROKE]])
         for p in points:
-            q[X] = p[X] * size / box[X]
-            q[Y] = p[Y] * size / box[Y]
+            q[X] = p[X] * SIZE / box[X]
+            q[Y] = p[Y] * SIZE / box[Y]
             new_points = np.concatenate((new_points, q), axis=0)
 
         return np.delete(new_points, 0)
@@ -130,5 +114,49 @@ class OneDollar():
         return np.delete(new_points, 0)
 
     def recognize(self, points, templates):
-        b =
->>>>>>> Stashed changes
+        b = float('inf')
+        for t in templates:
+            d = self.distance_at_best_angle(points, t, -PI_BY_4, PI_BY_4, PI_BY_90)
+            if d < b:
+                b = d
+                t1 = t
+        score = (1 - b) / (0.5 * math.sqrt(2 * (SIZE ** 2)))
+        return [t1, score]
+
+    def distance_at_best_angle(self, points, t, θa, θb, θd):
+        x1 = PHI * θa + (1 - PHI) * θb
+        f1 = distance
+        x2 = (1 - PHI) * θa + PHI * θb
+        f2 = self.distance_at_angle(points, t, x2)
+
+        while(abs(θb - θa) > θd):
+            if f1 < f2:
+                θb = x2
+                x2 = x1
+                f2 = f1
+                x1 = PHI * θa + (1 - PHI) * θb
+                f1 = self.distance_at_angle(points, t, x1)
+
+            else:
+                θa = x1
+                x1 = x2
+                f1 = f2
+                x2 = (1 - PHI) * θa + PHI * θb
+                f2 = self.distance_at_angle(points, t, x2)
+
+        return min(f1, f2)
+
+    def distance_at_angle(self, points, t, θ):
+        new_points = self.rotate_by(points, θ)
+        return self.path_distance(new_points, t)
+
+    def path_distance(self, a, b):
+        d = 0
+        for i in range(0, abs(a)):
+            d += distance(a[i], b[i])
+
+        return d/abs(a)
+
+
+
+
