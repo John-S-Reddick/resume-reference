4839f05adff4155c1df363aab10841cd010fbaa9
John-S-Reddick <154930148+John-S-Reddick@users.noreply.github.com>
2024-02-12 16:21:39 -0500

    Refined All gui elements, and fixed One Dollar completely

--- CIS4930 Novel User Interfaces/Project 1/components.py
+++ CIS4930 Novel User Interfaces/Project 1/components.py
@@ -1,205 +1,132 @@
+import tkinter
 from tkinter import *
 import numpy as np
 import tkinter as tk
+import main as m
 from os.path import exists
 from tempfile import TemporaryFile
 import recognizers as r
-
-WINDOW_SCALER = .5
-CANVAS_SCALER = 1
-GAP_SCALER = 1 - CANVAS_SCALER / 4
+import data as d
 
 # Numbers involved in Entry creation
-X = 0
-Y = 1
-STROKE = 2
-DIMENSIONS = 3
-IGNORE = -1
+X = m.X
+Y = m.Y
+STROKE = m.STROKE
+DIMENSIONS = m.DIMENSIONS
+IGNORE = -m.IGNORE
+FIRST = m.FIRST
 
-WIDTH = 1920
-HEIGHT = 1080
+WIDTH = m.WIDTH
+HEIGHT = m.HEIGHT
 
-CANV_WIDTH = WIDTH * WINDOW_SCALER
-CANV_HEIGHT = HEIGHT * WINDOW_SCALER
+CANV_WIDTH = m.CANV_WIDTH
+CANV_HEIGHT = m.CANV_HEIGHT
 
-CANV_SIDE = CANV_WIDTH
+CANV_SIDE = m.CANV_SIDE
 
 # Symbols as required by Dr. Pittman
-SYMBOLS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '.', 't', 'a', 'n', 's', 'c', 'i']
-DIRECTORY = 'templates/'
-SAMPLES = 5
+SYMBOLS = m.SYMBOLS
+DIRECTORY = m.DIRECTORY
+NAME = m.NAME
+EXTENSION = m.EXTENSION
+SAMPLES = m.SAMPLES
+
+RESAMPLE_N = m.RESAMPLE_N
 
 
-class DrawingArea:
-    def __init__(self, wind, master, nphandler=None):
-        self.wind = wind
-        self.master = master
-        if nphandler:
-            self.np_handler = nphandler
-        self.points = np.array([[-1, -1, -1]])
-        self.undo_stack = []
-        self.redo_stack = []
-        self.x1 = IGNORE
-        self.y1 = IGNORE
-        self.stroke_count = -1
-        self.canvas = tk.Canvas(master, width=CANV_SIDE, height=CANV_SIDE, bg='white')
-        self.binds()
-        self.canvas.pack
-        self.buttons = DrawingButtons(master=self.master, canvas=self)
-        self.draw_prompt()
-        ###
-        """ Drawing """
+class DrawingArea2:
+    def __init__(self, director):
+        self.director = director
+        self.data = director.data
+        self.s = director.settings
+        self.dra = (0, 0, 0, 0)
 
-    ###
-    def draw_prompt(self):
-        s = str(self.np_handler.current_symbol())
-        self.canvas.create_text(
-            (300, 100),
-            text="Please draw: " + s,
-            font='tkDefaeultFont 24')
-
-    def painting(self, x2, y2):
-        if self.x1 == -1 or self.y1 == -1:
-            self.x1 = x2
-            self.y1 = y2
-        self.canvas.create_line(self.x1, self.y1, x2, y2)
-        self.x1 = x2
-        self.y1 = y2
-
-    def load_sketch(self, sketch):
-        self.clear_canvas()
-        line1 = -1
-
-        for line2 in sketch:
-            if line1 != line2[STROKE]:
-                self.x1 = -1
-                self.y1 = -1
-            self.painting(line2[X], line2[Y])
-            line1 = line2[STROKE]
-
-    def drag_handler(self, event):
-        self.painting(event.x, event.y)
-        newpoint = np.array([[event.x, event.y, self.stroke_count]])
-        self.points = np.concatenate((self.points, newpoint), axis=0)
-
-    def erase_sketch(self, *event):
-        self.points = np.array([[-1, -1, -1]])
-        self.undo_stack.append(self.points)
-        self.stroke_count = -1
-        self.clear_canvas()
+        self.canvas = tk.Canvas(
+            self.director.frame,
+            width=self.s['width'],
+            height=self.s['height'],
+            bg=self.s['bg'])
 
     def clear_canvas(self):
-        self.x1 = -1
-        self.y1 = -1
-        self.canvas.create_rectangle((0, 0), (CANV_SIDE, CANV_SIDE), fill='white')
-        self.draw_prompt()
-        ###
-        """ Data Management """
+        self.canvas.create_rectangle(
+            (0, 0),
+            (self.s['width'],
+             self.s['height']),
+            fill=self.s['bg'])
 
-    ###
-    def undo(self, *event):
-        if self.undo_stack:
-            self.redo_stack.append(self.points)
-            self.points = self.undo_stack.pop()
-            self.stroke_count = self.stroke_count - 1
+    def load_sketch(self, connect_dots=True):
+        self.sketch(self.data.points, connect_dots)
 
+    def sketch(self, points, connect_dots=True):
+        self.clear_canvas()
+        p1 = points[FIRST]
+        if connect_dots:
+            for p2 in points:
+                if p1[STROKE] == p2[STROKE]:
+                    self.canvas.create_rectangle(
+                        p1[X], p1[Y],
+                        p2[X], p2[Y]
+                    )
+                p1 = p2
         else:
-            self.clear_canvas()
+            for p in points:
+                self.canvas.create_rectangle(
+                    p[X] - self.s['point_width'],
+                    p[Y] - self.s['point_width'],
+                    p[X], p[Y]
+                )
 
-        self.load_sketch(self.points)
-
-    def redo(self, *event):
-        if self.redo_stack:
-            self.undo_stack.append(self.points)
-            self.points = self.redo_stack.pop()
-            self.stroke_count = self.stroke_count + 1
-
-        self.load_sketch(self.points)
-
-    def submit(self, *event):
-        # trigger training
-        if self.np_handler:
-            self.np_handler.increment(self.points)
-            self.erase_sketch()
-        # Trigger recognition
-        else:
-            self.wind.new_window()
-
-        """ User Interaction """
-
-    ##
-    def mouse_up(self, event):
-        self.redo_stack.append(self.points)
-
-    def mouse_click(self, event):
-        self.stroke_count += 1
-        self.x1 = -1
-        self.y1 = -1
-        self.undo_stack.append(self.points)
-        print(self.points)
-
-    def binds(self):
-        # Key Binding tutorial
-        # https://www.tutorialexample.com/python-tkinter-bind-ctrlkey-python-tutorial/
-        # https://dafarry.github.io/tkinterbook/tkinter-events-and-bindings.htm
-        self.canvas.bind("<Button-1>", self.mouse_click)
-        self.canvas.bind("<Button-2>", self.undo)
-        self.canvas.bind("<Button-3>", self.submit)
-        self.canvas.bind("<B1-Motion>", self.drag_handler)
-        self.canvas.bind_all("<Control-Key-z>", self.undo)
-        self.canvas.bind_all("<Control-Key-y>", self.redo)
-        self.canvas.bind_all("<Control-Key-x>", self.erase_sketch)
-        self.canvas.bind_all("<Return>", self.submit)
-        self.canvas.pack()
-        # self.bind("<ButtonRelease-1>", mouse_up)
-
-        # subButton = Button(root, text="Submit", command=save)
-        # subButton.grid(column=0, row=1)
-
-## TODO THIS THING! DRAWS THE POINTS> DOES MATH> ALMOST DONE!
-## ALMOST DONE!
-class TemplateGuy:
-
-    def __init__(self, wind, master, nphandler=None):
-        self.wind = wind
-        self.master = master
-        if nphandler:
-
-        self.canvas = tk.Canvas(master, width=CANV_SIDE, height=CANV_SIDE, bg='white')
-        self.draw_prompt()
-
-    ###
-    def draw_prompt(self):
-        s = str(self.np_handler.current_symbol())
+    def draw_text(self):
         self.canvas.create_text(
-            (300, 100),
-            text="Please draw: " + s,
-            font='tkDefaeultFont 24')
+            (200, 200),
+            text=self.director.text,
+            font=self.s['font']
+        )
 
-    def painting(self, x2, y2):
-        if self.x1 == -1 or self.y1 == -1:
-            self.x1 = x2
-            self.y1 = y2
-        self.canvas.create_line(self.x1, self.y1, x2, y2)
-        self.x1 = x2
-        self.y1 = y2
+    def paint(self):
+        l = self.data.points.shape[0] - 1
+        if l > 0:
+            p1 = self.data.points[l - 1]
+            p2 = self.data.points[l]
+            if p1[STROKE] == p2[STROKE]:
+                self.canvas.create_rectangle(
+                    p1[X], p1[Y],
+                    p2[X], p2[Y]
+                )
 
-class DrawingButtons:
-    def __init__(self, master, canvas):
 
-        if not canvas.np_handler.training:
-            subtext = 'Recognize Sketch'
-        else:
-            subtext = 'Save template'
-        undo_button = Button(master, text="Undo", command=canvas.undo)
-        redo_button = Button(master, text="Redo", command=canvas.redo)
-        erase_button = Button(master, text="Clear Canvas", command=canvas.erase_sketch)
-        submit_button = Button(master, text=subtext, command=canvas.submit)
+class Binds:
+    def __init__(self, director, element, mode):
+        canvas = element.canvas
+        frame = director.frame
+        canvas.bind("<Button-1>", director.mouse_down)
+        canvas.bind("<Button-2>", director.undo)
+        canvas.bind("<B1-Motion>", director.paint)
+        canvas.bind_all("<Control-Key-z>", director.undo)
+        canvas.bind_all("<Control-Key-y>", director.redo)
+        canvas.bind_all("<Control-Key-x>", director.erase_sketch)
+        canvas.bind_all("<ButtonRelease-1>", director.mouse_up)
 
-        undo_button.pack()
-        redo_button.pack()
-        erase_button.pack()
-        submit_button.pack()
+        undo_button = Button(director.frame, text="Undo", command=director.undo)
+        redo_button = Button(director.frame, text="Redo", command=director.redo)
+        erase_button = Button(director.frame, text="Clear Canvas", command=director.erase_sketch)
+
+        if mode == "Training":
+            canvas.bind("<Button-3>", director.write_template)
+            # self.element.bind("<Return>", director.write_template)
+            submit_button = Button(
+                director.frame,
+                text="Submit Template",
+                command=director.write_template)
+
+        if mode == "Evaluation":
+            canvas.bind("<Button-3>", director.recognize)
+            # self.element.bind("<Return>", director.resample)
+            # self.element.bind("<Return>", director.recognize)
+            submit_button = Button(
+                director.frame,
+                text="Recognize",
+                command=director.recognize)
 
 
 class Welcome:
@@ -228,74 +155,27 @@ class Welcome:
     def pre_trained(self):
         self.newWindow = tk.Toplevel(self.master)
 
-        self.app = Evaluation(self)
-
+        self.app = SketchWindow(self.newWindow, option='recognition')
 
     def to_train(self):
         self.newWindow = tk.Toplevel(self.master)
 
-        self.app = Training(self.newWindow)
+        self.app = SketchWindow(self.newWindow, option='training')
 
 
-class Training:
-    def __init__(self, master):
+class SketchWindow:
+    def __init__(self, master, option='recognition'):
         self.app = None
         self.newWindow = None
         self.master = master
 
         self.frame = tk.Frame(self.master)
+        self.director = d.Director(self.frame)
 
-        self.canvas = DrawingArea(
-            wind=self,
-            master=self.frame,
-            nphandler=TrainingHandler()
-        )
+        if option == 'recognition':
+            self.director.enter_recogntion()
+
+        if option == 'training':
+            self.director.enter_training()
+
         self.frame.pack()
-
-    def new_window(self):
-        self.newWindow = tk.Toplevel(self.master)
-        self.app = Evaluation(self.newWindow)
-
-
-class TrainingHandler:
-    def __init__(self, symbols=SYMBOLS, samples=SAMPLES):
-        self.training = True
-        self.symbols = symbols
-        self.samples = samples
-        self.inc = 0
-        self.itr = 0
-        self.entry_names = []
-        for s in symbols:
-            self.entry_names.append(DIRECTORY + 'tmplt.' + s + '.')
-
-    # Keywords numpy savez
-    # https://stackoverflow.com/questions/33878179/use-value-of-variable-rather-than-keyword-in-python-numpy-savez
-    def ind(self):
-        return int(self.inc / self.samples)
-
-    def current_symbol(self):
-        return self.symbols[self.ind()]
-
-    def increment(self, points):
-        index = self.ind()
-        if index < len(self.symbols):
-            current_entry = self.entry_names[index]
-            path = current_entry + str(int(self.inc) % int(self.samples)) + ".npy"
-            with open(path, 'wb') as f:
-                np.save(f, points)
-            print("Saved to: " + path)
-            self.inc += 1
-        else:
-            self.training = False
-
-
-class Evaluation:
-    def __init__(self, master):
-        self.master = master
-        self.frame = tk.Frame(self.master)
-        self.canvas = DrawingArea()
-        self.display = TemplateGuy()
-        self.frame.pack()
-
-    def close_windows(self):
-        self.master.destroy()
--- CIS4930 Novel User Interfaces/Project 1/recognizers.py
+++ CIS4930 Novel User Interfaces/Project 1/recognizers.py
@@ -1,135 +1,200 @@
 import math
-import numpy as np
 
+import numpy
+import numpy as np
+import main as m
 
 PHI = .5 * (-1 + math.sqrt(5))
-PI_BY_4 = math.pi / 4       # θ = ±45°
-PI_BY_90 = math.pi / 90     # θ∆ = 2°
+PI_BY_4 = math.pi / 4  # θ = ±45°
+PI_BY_90 = math.pi / 90  # θ∆ = 2°
 # Point Access
-X = 0
-Y = 1
+X = int(0)
+Y = int(1)
 STROKE = 2
 
-K = [0,0]
+K = [0, 0]
 SIZE = 25
 
 # List aspects
 FIRST = 0
+SECOND = 1
 DIMENSIONS = 3
+
+RESAMPLE_N = m.RESAMPLE_N
+
+BBOX_SIZE = m.BBOX_SIZE
+
+
+def arr_size(points):
+    return int((points.size - 1) / 3)
+
+
+def sanitize(points):
+    # Find indices where values are not [-1, -1, -1]
+    indices = np.any(points != [-1, -1, -1], axis=1)
+    # Filter the array using the indices
+    return points[indices]  # .astype(numpy.float32)
 
 
 # Outputs the euclidean distance between two entries in a numpy array
 def distance(p2, p1):
-    # sqrt((x2 - x1)^2 + (y2 - y1)^2))
     return math.sqrt((p2[X] - p1[X]) ** 2 + (p2[Y] - p2[Y]) ** 2)
+
 
 def centroid(points):
     # Uses Numpy mean
     # https://numpy.org/doc/stable/reference/generated/numpy.mean.html
-    c = np.array([[X, Y, STROKE]])
-    c[X] = np.mean(points, where=[[True], [False], [False]])
-    c[Y] = np.mean(points, where=[[False], [True], [False]])
+    c = np.array([X, Y, STROKE])
+    inc = 0
+    # for i in points:
+    #    inc += 1
+    #    x = i[X]
+    #    y = i[Y]
+    #
+    # x /= inc
+    # y /= inc
+    c[X] = np.mean(points, where=[True, False, False])
+    c[Y] = np.mean(points, where=[False, True, False])
+    # return np.array([x,y,0])
     return c
 
-#TODO Implement bounding_box
+
+# TODO Implement bounding_box
 def bounding_box(points):
-    return [1,2]
+    x_coords = points[:, 0]  # Extract x coordinates
+    y_coords = points[:, 1]  # Extract y coordinates
+
+    x1 = np.min(x_coords)  # Minimum x coordinate
+    x2 = np.max(x_coords)  # Maximum x coordinate
+
+    y1 = np.min(y_coords)  # Minimum y coordinate
+    y2 = np.max(y_coords)  # Maximum y coordinate
+
+    width = x2 - x1  # Width of the bounding box
+    height = y2 - y1  # Height of the bounding box
+
+    return [width, height]
+
+
+class RecognitionManager():
+    def __init__(self):
+        pass
+
 
 class OneDollar():
-    # Psuedo code for class found here:
-    #
+    def __init__(self, templates):
+        self.templates = []
+        for t in templates:
+            t = self.resample(t, RESAMPLE_N)
+            t = self.rotate_to_zero(t)
+            t = self.scale_to_square(t)
+            t = self.translate_to_origin(t)
+            self.templates.append(t)
+
+    def prepare_templates(self, templates, n):
+        temp = []
+        for t in templates:
+            temp.append(self.resample(t, n))
+        return temp
+
     def resample(self, points, n):
+        points = sanitize(points)
         I = self.path_length(points) / (n - 1)
         D = 0
 
-        p1 = points[FIRST]
-        new_points = np.array([points[FIRST]])
+        new_points = np.zeros((n, 3))
+        new_points[0] = points[FIRST]
 
-        for p2 in points:
-            if p2 == p1:
-                continue
-
-            d = distance(p1, p2)
+        q = points[FIRST]
+        x = 0
+        for i in range(1, arr_size(points)):
+            pi = points[i]
+            p1 = points[i - 1]
+            d = distance(p1, pi)
             if D + d >= I:
-                c = (I - D) / d
-                p1x = p1[X] + c * (p2[X] - p1[X])
-                p1y = p1[Y] + c * (p2[Y] - p1[Y])
-                p1 = np.array([[p1x, p1y, 0]])
+                eq = (I - D) / d
+                q[X] = p1[X] + eq * (pi[X] - p1[X])
+                q[Y] = p1[Y] + eq * (pi[Y] - p1[Y])
 
-                new_points = np.concatenate((new_points, p1), axis=0)
-
+                new_points[x] = q
+                x += 1
+                points[i-1] = np.array([q])
                 D = 0
-
             else:
                 D = D + d
-
+        print(arr_size(new_points))
         return new_points
+
     def path_length(self, points):
         d = 0
-        p1 = points[FIRST]
-        for p2 in points:
-            if p1 == p2:
-                continue
-            d += distance(p2, p1)
-            p1 = p2
-
+        for i in range(1, arr_size(points)):
+            d += distance(points[i - 1], points[i])
         return d
 
-    def indicative_angle(self, points):
+    def rotate_to_zero(self, points):
         c = centroid(points)
         p = points[FIRST]
-        return math.atan2(c[Y] - p[Y], c[X] - p[X])
+        θ = math.atan2(c[Y] - p[Y], c[X] - p[X])
+        return self.rotate_by(points, -1 * θ)
 
     def rotate_by(self, points, w):
         new_points = np.array([[X, Y, STROKE]])
         c = centroid(points)
         q = np.array([[X, Y, STROKE]])
         for p in points:
-            q[X] = (p[X] - c[X]) * math.cos(w) - (p[Y] - c[Y]) * math.sin(w) + c[X]
-            q[Y] = (p[X] - c[X]) * math.sin(w) - (p[Y] - c[Y]) * math.cos(w) + c[Y]
+            q[FIRST][X] = (p[X] - c[X]) * math.cos(w) - (p[Y] - c[Y]) * math.sin(w) + c[X]
+            q[FIRST][Y] = (p[X] - c[X]) * math.sin(w) - (p[Y] - c[Y]) * math.cos(w) + c[Y]
             new_points = np.concatenate((new_points, q), axis=0)
 
-        return np.delete(new_points, 0)
+        return new_points
 
-    def scale_to(self, points):
+    def scale_to_square(self, points):
         box = bounding_box(points)
         new_points = np.array([[X, Y, STROKE]])
         q = np.array([[X, Y, STROKE]])
         for p in points:
-            q[X] = p[X] * SIZE / box[X]
-            q[Y] = p[Y] * SIZE / box[Y]
+            q[FIRST][X] = p[X] * SIZE / box[X]
+            q[FIRST][Y] = p[Y] * SIZE / box[Y]
             new_points = np.concatenate((new_points, q), axis=0)
 
-        return np.delete(new_points, 0)
+        return new_points
 
-    def translate_to(self, points, k):
+    def translate_to_origin(self, points):
         c = centroid(points)
-        new_points = np.array([[X, Y, STROKE]])
-        q = np.array([[X, Y, STROKE]])
+        new_points = np.array([[-1, -1, -1]])
+        q = np.array([X, Y, STROKE])
         for p in points:
-            q[X] = p[X] + K[X] - c[X]
-            q[Y] = p[Y] + K[Y] - c[Y]
-            new_points = np.concatenate((new_points, q), axis=0)
+            q[X] = p[X] - c[X]
+            q[Y] = p[Y] - c[Y]
+            new_points = np.concatenate((new_points, np.array([q])), axis=0)
 
-        return np.delete(new_points, 0)
+        return sanitize(new_points)
 
-    def recognize(self, points, templates):
+    def sketch(self, points):
+        points = self.resample(points, RESAMPLE_N)
+        points = self.rotate_to_zero(points)
+        points = self.scale_to_square(points)
+        points = self.translate_to_origin(points)
+        return self.recognize(points)
+
+    def recognize(self, points):
         b = float('inf')
-        for t in templates:
-            d = self.distance_at_best_angle(points, t, -PI_BY_4, PI_BY_4, PI_BY_90)
+        for t in range(len(self.templates)):
+            d = self.distance_at_best_angle(points, self.templates[t], -PI_BY_4, PI_BY_4, PI_BY_90)
             if d < b:
                 b = d
                 t1 = t
         score = (1 - b) / (0.5 * math.sqrt(2 * (SIZE ** 2)))
+        print([t1, score])
         return [t1, score]
 
     def distance_at_best_angle(self, points, t, θa, θb, θd):
         x1 = PHI * θa + (1 - PHI) * θb
-        f1 = distance
+        f1 = self.distance_at_angle(points, t, x1)
         x2 = (1 - PHI) * θa + PHI * θb
         f2 = self.distance_at_angle(points, t, x2)
 
-        while(abs(θb - θa) > θd):
+        while (abs(θb - θa) > θd):
             if f1 < f2:
                 θb = x2
                 x2 = x1
@@ -150,13 +215,18 @@ class OneDollar():
         new_points = self.rotate_by(points, θ)
         return self.path_distance(new_points, t)
 
+    # TODO Work on path distance
     def path_distance(self, a, b):
         d = 0
-        for i in range(0, abs(a)):
+        for i in range(RESAMPLE_N):
             d += distance(a[i], b[i])
-
-        return d/abs(a)
-
-
+        #
+        return d / RESAMPLE_N
 
 
+class PennyPincher():
+    def __init__(self):
+        pass
+
+    def resample(self):
+        pass
